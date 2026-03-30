692e9b5fb30c652ccffbe78c3c4a17f2b367dd85
croi900 <cristianalexandrucroitoru@gmail.com>
2026-03-30 16:56:26 +0300

    luni 30 martie 2026, 16:56:26 +0300

--- src/tokenizer.hpp
+++ src/tokenizer.hpp
@@ -8,8 +8,22 @@
 namespace idf {
     inline std::vector<TokenInfo> tokenize_chunk(std::string_view content, uint64_t base_offset) {
         std::vector<TokenInfo> tokens;
-        tokens.reserve(content.size() / 8); 
+        tokens.reserve(content.size() / 8);
+
+#ifdef __AVX512BW__
+
+
+
+        static const bool have_avx512bw = __builtin_cpu_supports("avx512bw");
+        if (have_avx512bw) {
+            tokenize_simd_avx512(content, base_offset, tokens);
+        } else {
+            tokenize_simd_avx2(content, base_offset, tokens);
+        }
+#else
         tokenize_simd_avx2(content, base_offset, tokens);
+#endif
+
         return tokens;
     }
 }
