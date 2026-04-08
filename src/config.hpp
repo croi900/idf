e786e394e83bbbeebb6e8fd40597df8d059d8546
croi900 <cristianalexandrucroitoru@gmail.com>
2026-04-08 03:20:41 +0300

    forgot this

---
+++ src/config.hpp
@@ -0,0 +1,27 @@
+#ifndef IDF_CONFIG_HPP
+#define IDF_CONFIG_HPP
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <hpx/hpx.hpp>
+#include "src/extensions.hpp"
+
+namespace idf::config {
+    inline size_t num_workers = 0;
+    inline size_t chunk_size = 32 * 1024 * 1024;
+    inline size_t max_active_chunks = 1024;
+    inline std::vector<std::string> allowed_extensions(dirtree::text_extensions.begin(), dirtree::text_extensions.end());
+    inline size_t num_shards = 256;
+    inline std::string root_path = ".";
+
+    inline bool is_allowed_extension(const std::string_view& ext) {
+        if (allowed_extensions.empty()) return true;
+        for (const auto& allowed : allowed_extensions) {
+            if (ext == allowed) return true;
+        }
+        return false;
+    }
+}
+
+#endif //IDF_CONFIG_HPP
